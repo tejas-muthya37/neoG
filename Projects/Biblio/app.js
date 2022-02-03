f93606344bad0d74145ecc0c68dd7462962639d4
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-02-03 12:48:45 +0530

    routine

--- Projects/Biblio/app.js
+++ Projects/Biblio/app.js
@@ -2,10 +2,8 @@ const slideFunction = function () {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-items");
   const items = document.querySelectorAll(".nav-items li");
-  const notNav = document.querySelector(".landing-page-container");
 
   burger.addEventListener("click", function () {
-    notNav.classList.toggle("nav-open-opacity");
     nav.classList.toggle("navSlide");
 
     items.forEach((item, index) => {
